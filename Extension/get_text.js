323d9a2820eca1466499306988ea2b786ebea909
Dovie Weinstock <32934685+DovieW@users.noreply.github.com>
2023-02-17 08:46:16 -0500

    go directly to urls

--- Extension/get_text.js
+++ Extension/get_text.js
@@ -1,6 +1,5 @@
 document.querySelector('body').addEventListener('mouseup', (e) => {
   if (e.button === 1){
-    console.log('MIDDLE CLICK');
     if (window.getSelection) {
       text = window.getSelection().toString();
     } else if (document.selection && document.selection.type != "Control") {
